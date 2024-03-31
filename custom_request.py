bac1a87c733865f2eb5b3cec3d089199c0b3c400
IvanZakharovIVT <ivanzahna@gmail.com>
2024-03-31 14:50:02 +0400

    Реструктуризация проекта

---
+++ custom_request.py
@@ -0,0 +1,28 @@
+import time
+
+import requests
+
+from custom_exception import MyException
+
+
+class CustomRequest:
+    def __init__(self):
+        self.sess = requests.Session()
+        headers = {
+            'Content-type': 'text/html;charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
+        }
+        self.sess.headers.update(headers)
+
+    def get(self, request_url: str) -> str:
+        return self._do_request(request_url)
+
+    def _do_request(self, request_url: str) -> str:
+        try_count = 0
+        while try_count < 5:
+            some_result = self.sess.get(request_url)
+            if some_result.status_code == 200:
+                return some_result.text
+            try_count += 1
+            time.sleep(2)
+        raise MyException("Превышено количество запросов")
--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-import requests
 from bs4 import BeautifulSoup, Tag
 import re
 import time
 import xmltodict
-import celery
 
+from custom_exception import MyException
+from custom_request import CustomRequest
 
-class MyException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
-sess = requests.Session()
-headers = {
-    'Content-type': 'text/html;charset=UTF-8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
-}
-sess.headers.update(headers)
 
 main_address = "https://zakupki.gov.ru"
 search_address = "/epz/order/extendedsearch/results.html"
 paginator_suffix = "?fz44=on&pageNumber="
 
 pattern = re.compile(r'/epz/.+printForm/view.html.+regNumber=\d+')
-# some_result = requests.get("https://zakupki.gov.ru/epz/order/extendedsearch/results.html")
 
 
 def convert_url(inner_url: Tag) -> str:
@@ -32,20 +20,11 @@ def convert_url(inner_url: Tag) -> str:
     return f"{main_address}{href}"
 
 
-def do_request(request_url: str) -> str:
-    try_count = 0
-    while try_count < 5:
-        some_result = sess.get(request_url)
-        if some_result.status_code == 200:
-            return some_result.text
-        try_count += 1
-        time.sleep(2)
-    raise MyException("Превышено количество запросов")
-
+my_request = CustomRequest()
 
 page_request_href = f"{main_address}{search_address}{paginator_suffix}{1}"
 
-some_result = do_request(page_request_href)
+some_result = my_request.get(page_request_href)
 
 soup = BeautifulSoup(some_result, 'html.parser')
 
@@ -56,7 +35,7 @@ reg1 = r"publishDTInEIS\>.+\<"
 reg2 = r"\d.+\d"
 
 for item in test_arr:
-    request = do_request(item)
+    request = my_request.get(item)
     btf = xmltodict.parse(request)
     publishDTInEIS = None
     level1 = btf.get('ns7:epNotificationEZT2020')
